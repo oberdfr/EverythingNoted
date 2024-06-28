7e9b4ab2b09cc63f1fc37018953b0ac475af372b
Oberd <112550174+oberdfr@users.noreply.github.com>
2024-06-28 20:04:45 +0200

    ShoppingList: fix 'sealed' sub-object can be converted to 'data object'

--- app/src/main/java/com/example/shoppinglist/bottomnavbar/BottomNavItem.kt
+++ app/src/main/java/com/example/shoppinglist/bottomnavbar/BottomNavItem.kt
@@ -8,25 +8,25 @@ sealed class BottomNavItem(
     var title: String,
     var icon: Int
 ) {
-    object Shopping :
+    data object Shopping :
         BottomNavItem(
             "Shopping",
             R.drawable.shoppingcart
         )
 
-    object Notes :
+    data object Notes :
         BottomNavItem(
             "Notes",
             R.drawable.notes_24px
         )
 
-    object Todo :
+    data object Todo :
         BottomNavItem(
             "Todo",
             R.drawable.calendar_month_24px
         )
 
-    object People :
+    data object People :
         BottomNavItem(
             "People",
             R.drawable.contacts_product_24px
